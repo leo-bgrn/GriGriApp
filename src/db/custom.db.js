675805ae01a64c33031b7062247bb4ebfc1ad349
BONGIORNO <bongiorno.leo@gmail.com>
2020-11-12 16:07:34 +0100

    Add historic with user name

--- src/db/custom.db.js
+++ src/db/custom.db.js
@@ -4,12 +4,22 @@ const logger = require("../../config/winston");
 const getLast2UsersWithGrigriQuery =
   "SELECT grigri_location.user as user_id, grigri_location.from, users.name FROM grigri_location INNER JOIN users ON users.id = grigri_location.user ORDER BY grigri_location.from DESC LIMIT 2";
 
+const getHistoricWithUsersNameQuery =
+  "SELECT users.name, grigri_location.from, grigri_location.to FROM grigri_location INNER JOIN users ON grigri_location.user = users.id ORDER BY grigri_location.from DESC";
+
 async function getLast2UsersWithGrigri() {
   const last2Users = await db.query(getLast2UsersWithGrigriQuery);
   logger.info("Get last 2 users with grigri from database :" + last2Users);
   return last2Users;
 }
 
+async function getHistoricWithUsersName() {
+  const historic = await db.query(getHistoricWithUsersNameQuery);
+  logger.info("Get Historic with users :" + historic);
+  return historic;
+}
+
 module.exports = {
   getLast2UsersWithGrigri,
+  getHistoricWithUsersName,
 };
--- src/routes/custom.route.js
+++ src/routes/custom.route.js
@@ -1,11 +1,21 @@
 let express = require("express");
 let router = express.Router();
 const customController = require("../controllers/custom.controller");
+const customDb = require("../db/custom.db");
 
 router.get("/currentInfo", async function (req, res, next) {
   try {
     const currentInfo = await customController.getCurrentInfo();
     res.send(currentInfo);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/historic", async function (req, res, next) {
+  try {
+    const historic = await customDb.getHistoricWithUsersName();
+    res.send(historic);
   } catch (e) {
     next(e);
   }
