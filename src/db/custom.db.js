ff30d1e0e1328b21b461246375f4281849482341
BONGIORNO <bongiorno.leo@gmail.com>
2020-11-17 18:35:46 +0100

    fix issue

--- src/db/custom.db.js
+++ src/db/custom.db.js
@@ -2,10 +2,10 @@ const db = require("./db");
 const logger = require("../../config/winston");
 
 const getLast2UsersWithGrigriQuery =
-  "SELECT grigri_location.user as user_id, grigri_location.from, users.name FROM grigri_location INNER JOIN users ON users.id = grigri_location.user ORDER BY grigri_location.from DESC LIMIT 2";
+  "SELECT grigri_location.user as user_id, grigri_location.from, users.name, users.avatar FROM grigri_location INNER JOIN users ON users.id = grigri_location.user ORDER BY grigri_location.from DESC LIMIT 2";
 
 const getHistoricWithUsersNameQuery =
-  "SELECT users.name, grigri_location.from, grigri_location.to FROM grigri_location INNER JOIN users ON grigri_location.user = users.id ORDER BY grigri_location.from DESC";
+  "SELECT users.name, grigri_location.from, grigri_location.to, users.avatar FROM grigri_location INNER JOIN users ON grigri_location.user = users.id ORDER BY grigri_location.from DESC";
 
 async function getLast2UsersWithGrigri() {
   const last2Users = await db.query(getLast2UsersWithGrigriQuery);
